a062e3624d2603ecd1c264dc1ae264ccc5a3d82a
MonicaMMartinezV <111510324+MonicaMMartinezV@users.noreply.github.com>
2022-11-27 05:30:52 -0600

    Add files via upload

--- Code/AgentePolicia.cpp
+++ Code/AgentePolicia.cpp
@@ -6,21 +6,53 @@ using namespace std;
 AgentePolicia::AgentePolicia()
 {}
 
-AgentePolicia::~AgentePolicia()
-{}
-
-void AgentePolicia::agregarDelct(long long int delId,
-                  long long int horasAsignadas)
+AgentePolicia::AgentePolicia(long long int _id,
+                             string        _nombre,
+                             string        _genero,
+                             string        _puesto,
+                             long long int _telefono,
+                             int           _ansTrabajo,
+                             Celda         _celAsg,
+                             long long int _numeroDel) : 
+                             Personal(_id,
+                                      _nombre,
+                                      _genero,
+                                      _puesto,
+                                      _telefono,
+                                      _ansTrabajo)
 {
-    celAsg.guardarCelda(delId, horasAsignadas);
+    celAsg = _celAsg;
+    numeroDel = _numeroDel;
 }
 
-void AgentePolicia::sacarDelct(long long int delId)
+void AgentePolicia::agregarDelct(long long int numeroDel,
+                                 long long int horasAsignadas)
 {
-    celAsg.sacarDelCelda(delId);
+    celAsg.guardarCelda(numeroDel, horasAsignadas);
 }
 
-void AgentePolicia::verDatos()
+void AgentePolicia::sacarDelct(long long int numeroDel)
 {
-    cout << celAsg.verDatos();
+    celAsg.sacarDelCelda(numeroDel);
+}
+
+string AgentePolicia::getDatosAP()
+{
+    return getDatosP() + celAsg.getDatosC();
+}
+
+void AgentePolicia::setCelAsg(Celda _celAsg){
+    celAsg = _celAsg;
+}
+
+Celda AgentePolicia::getCelAsg(){
+    return celAsg;
+}
+
+void AgentePolicia::setNumeroDel(long long int _numeroDel){
+    numeroDel = _numeroDel;
+}
+
+long long int AgentePolicia::getNumeroDel(){
+    return numeroDel;
 }
